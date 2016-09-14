858c1d56d866dffe57a1419ecd8c833de60be011
Adam Venturella <aventurella@blitzagency.com>
2016-09-14 13:08:32 -0700

    refactor ChildViewContainer  to Container
    Allow for more flexability in the container.

--- marionette/3.0/src/child-view-container.d.ts
+++ marionette/3.0/src/child-view-container.d.ts
@@ -1,50 +1,51 @@
 /// <reference path="../../../backbone/backbone.d.ts" />
 
 
-declare namespace Backbone {
-    // Backbone.BabySitter
-    class ChildViewContainer<TView extends View<Backbone.Model>> {
 
-        constructor(initialViews?: any[]);
+declare namespace Marionette {
+    type BaseView = Marionette.View<Backbone.Model>
 
-        add(view: TView, customIndex?: number): void;
-        findByModel<TModel extends Backbone.Model>(model: TModel): TView;
-        findByModelCid(modelCid: string): TView;
-        findByCustom(index: number): TView;
-        findByIndex(index: number): TView;
-        findByCid(cid: string): TView;
-        remove(view: TView): void;
-        call(method: any): void;
-        apply(method: any, args?: any[]): void;
+    class Container {
 
-        //mixins from Collection (copied from Backbone's Collection declaration)
+        constructor(initialViews?: BaseView[]);
 
-        all(iterator: (element: TView, index: number) => boolean, context?: any): boolean;
-        any(iterator: (element: TView, index: number) => boolean, context?: any): boolean;
+        add(view: BaseView, customIndex?: number): void;
+        findByModel<TModel extends Backbone.Model>(model: TModel): View<TModel>;
+        findByModel(model: any): any;
+        findByModelCid(modelCid: string): BaseView;
+        findByCustom(index: number): BaseView;
+        findByIndex(index: number): BaseView;
+        findByCid(cid: string): BaseView;
+        remove(view: BaseView): void;
+
+        // mixins from underscore (copied from underscore)
+
+        all(iterator: (element: BaseView, index: number) => boolean, context?: any): boolean;
+        any(iterator: (element: BaseView, index: number) => boolean, context?: any): boolean;
         contains(value: any): boolean;
         detect(iterator: (item: any) => boolean, context?: any): any;
-        each(iterator: (element: TView, index: number, list?: any) => void, context?: any): any;
-        every(iterator: (element: TView, index: number) => boolean, context?: any): boolean;
-        filter(iterator: (element: TView, index: number) => boolean, context?: any): TView[];
-        find(iterator: (element: TView, index: number) => boolean, context?: any): TView;
-        first(): TView;
-        forEach(iterator: (element: TView, index: number, list?: any) => void, context?: any): void;
+        each(iterator: (element: BaseView, index: number, list?: any) => void, context?: any): any;
+        every(iterator: (element: BaseView, index: number) => boolean, context?: any): boolean;
+        filter(iterator: (element: BaseView, index: number) => boolean, context?: any): BaseView[];
+        find(iterator: (element: BaseView, index: number) => boolean, context?: any): BaseView;
+        first(): BaseView;
+        forEach(iterator: (element: BaseView, index: number, list?: any) => void, context?: any): void;
         include(value: any): boolean;
-        initial(): TView;
-        initial(n: number): TView[];
+        initial(): BaseView;
+        initial(n: number): BaseView[];
         invoke(methodName: string, args?: any[]): any;
         isEmpty(object: any): boolean;
-        last(): TView;
-        last(n: number): TView[];
-        lastIndexOf(element: TView, fromIndex?: number): number;
-        map<U>(iterator: (element: TView, index: number, context?: any) => U, context?: any): U[];
+        last(): BaseView;
+        last(n: number): BaseView[];
+        map<U>(iterator: (element: BaseView, index: number, context?: any) => U, context?: any): U[];
         pluck(attribute: string): any[];
-        reject(iterator: (element: TView, index: number) => boolean, context?: any): TView[];
-        rest(): TView;
-        rest(n: number): TView[];
+        reject(iterator: (element: BaseView, index: number) => boolean, context?: any): BaseView[];
+        rest(): BaseView;
+        rest(n: number): BaseView[];
+        reduce(iterator: any, memo?: any, context?: any);
         select(iterator: any, context?: any): any[];
-        some(iterator: (element: TView, index: number) => boolean, context?: any): boolean;
+        some(iterator: (element: BaseView, index: number) => boolean, context?: any): boolean;
         toArray(): any[];
-        without(...values: any[]): TView[];
+        without(...values: any[]): BaseView[];
     }
 }
