dd94591241b0d958654de9d8e3139fc5a49cf201
Adam Venturella <aventurella@blitzagency.com>
2016-09-14 08:30:50 -0700

    update AppRouter

--- marionette/3.0/src/app-router.d.ts
+++ marionette/3.0/src/app-router.d.ts
@@ -1,15 +1,9 @@
 /// <reference path="../../../backbone/backbone.d.ts" />
 
 declare namespace Marionette{
-    interface AppRouterOptions extends Backbone.RouterOptions {
-        /**
-         * The appRoutes.
-         */
-        appRoutes?: any;
 
-        /**
-         * The controller to associate with this router.
-         */
+    interface AppRouterOptions extends Backbone.RouterOptions {
+        appRoutes?: any;
         controller?: any;
     }
 
@@ -18,7 +12,7 @@ declare namespace Marionette{
      * single method on another object. Have your routers configured to call
      * the method on your object, directly.
      */
-    class AppRouter extends Backbone.Router {
+    class AppRouter extends Backbone.Router implements CommonMixins{
 
         /**
          * Configure an AppRouter with appRoutes. The route definition
@@ -30,18 +24,26 @@ declare namespace Marionette{
          */
         constructor(options?: AppRouterOptions);
 
-        /**
-         * You can specify a controller with the multiple routes at runtime with
-         * this method. However, In this case the current controller of AppRouter
-         * will not change.
-         */
-        processAppRoutes(controller: any, appRoutes: any): void;
+        // CommonMixins
+        normalizeMethods(hash: string): {[key: string]: any};
+        mergeOptions(options: any, keys: any): any;
+        getOption(optionName: string): any;
+        bindEvents(entity: any, bindings: any): any
+        unbindEvents(entity: any, bindings: any): any
 
         /**
-         * Adds an app route at runtime to this instance. It works the same as the
-         * built-in router.route() call from Backbone's Router, but has all the
-         * same semantics and behavior of the appRoutes configuration.
+         * Similar to route method on a Backbone Router but
+         * method is called on the controller
          */
-        appRoute(route: string, methodName: string): void;
+        appRoute(route: string, methodName: string): this;
+
+        /**
+         * Internal method to process the `appRoutes` for the
+         * router, and turn them in to routes that trigger the
+         */
+        processAppRoutes(controller: any, appRoutes: any): this;
+
+        triggerMethod(event: string, ...args: any[]): any;
+
     }
 }
